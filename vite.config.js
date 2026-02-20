b3ef09002cc131b30f86449402752d102f183f3e
Malak <malaksummer35@gmail.com>
2026-02-20 17:46:02 +0000

    Fix deployment: Change BrowserRouter to HashRouter for static hosting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/MyGraphicDesignPortfolio/' // <-- Add this line
+// })
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // <- This is important for GitHub Pages or subfolders
+});
